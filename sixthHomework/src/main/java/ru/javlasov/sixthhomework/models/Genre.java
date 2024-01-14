5318daaa89fb43afe04e836352834320a3fa7ecf
MaximVl <m.a.vlasov97@gmial.com>
2024-01-14 19:41:40 +0300

    [feature/sixthHomework] Edited for check style;
    - Edited style import for check style;

--- sixthHomework/src/main/java/ru/javlasov/sixthhomework/models/Genre.java
+++ sixthHomework/src/main/java/ru/javlasov/sixthhomework/models/Genre.java
@@ -1,7 +1,14 @@
 package ru.javlasov.sixthhomework.models;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "genres")
