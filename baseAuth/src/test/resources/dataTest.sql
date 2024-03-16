2bcfe2608b9dd2f1a193c1c15d58ac4b3bceb55b
MaximVl <m.a.vlasov97@gmial.com>
2024-03-16 21:00:31 +0300

    [feature/form-base-auth] Fixed after code review;
    - Edited tests scripts;

--- baseAuth/src/test/resources/dataTest.sql
+++ baseAuth/src/test/resources/dataTest.sql
@@ -4,4 +4,4 @@ MERGE INTO genres KEY (id, name) VALUES (1, 'Novel');
 
 MERGE INTO books KEY (id, title, genre_id, author_id) VALUES (1, 'Dead souls', 1, 1), (2, 'Crime and punishment', 2, 1);
 
-INSERT INTO users (id, username, password, role) VALUES (1, 'user', '$2a$12$WO1t.iy2F8H6kEXV2ARdLuyrHjqw.EaiqLkRczycg9R5tlmPIqsuO', 'USER'), (2, 'admin', '$2a$12$uEvrDZhniwfNYE4WoDBGLu6gkHSk4U.cj4UtuRrGFqFTHGllo/3zS', 'ADMIN') ON CONFLICT (id) DO NOTHING;
+MERGE INTO users KEY (id, username, password, role) VALUES (1, 'user', '$2a$12$WO1t.iy2F8H6kEXV2ARdLuyrHjqw.EaiqLkRczycg9R5tlmPIqsuO', 'USER'), (2, 'admin', '$2a$12$uEvrDZhniwfNYE4WoDBGLu6gkHSk4U.cj4UtuRrGFqFTHGllo/3zS', 'ADMIN');
